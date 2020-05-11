32a1ab16e866f29925a66821817521d1a80eeb45
knot-eye <ryanzhu.1@outlook.com>
2020-05-11 16:13:25 -0400

    Add movability & legal checking (check not included yet)

--- board.h
+++ board.h
@@ -1,19 +1,128 @@
-/*
- *
- * Version 1
- * May 11, 2020
- *
- */
-
+#include <vector>
 #include <string>
+#include <cmath>
+#include<iostream>
+
 class board {
- int pos[8][8][2],atk[8][8];
+ int pos[8][8][2],atk[8][8][2];
  public:
    std::string printBoard(void);
    void startGame(void);
    void setPos(int, int, int);
    void swp(int, int, int, int);
+   bool legal(int, int, int, int, int);
+   bool legal(int, int, int, int, std::vector<bool>);
+   std::vector<bool> getMoves(int);
+   int retLoc(int, int);
+   std::string printRaw(void);
 };
+
+std::string board::printRaw() {
+  std::string ret = "";
+  for (int i = 0; i < 8; i++) {
+    for (int a = 0; a < 8; a++) {
+      ret+= std::to_string(pos[i][a][0]) + " ";
+    }
+    ret+="\n";
+  }
+  return ret;
+}
+
+int board::retLoc(int x, int y) {
+  return pos[7-x][y][0];
+}
+
+std::vector<bool> board::getMoves(int id) {
+  bool diag,straight,horse,pawn,king;
+  diag = straight = horse = pawn = king = false;
+  switch (id) {
+    case 1:
+      pawn = true;
+      break;
+    case 2:
+      diag = true;
+      break;
+    case 3:
+      horse = true;
+      break;
+    case 4:
+      straight = true;
+      break;
+    case 5:
+      diag = true;
+      straight = true;
+      break;
+    case 6:
+      king = true;
+      break;
+  }
+  std::vector<bool> ret = {diag, straight, horse, pawn, king};
+  return ret;
+}
+
+bool board::legal(int x, int y, int a, int b, std::vector<bool> moves) {
+  if (pos[7-x][y][1] == pos[7-a][b][1]) return false;
+  if (moves[0]) {
+    if (abs(x-a) == abs(y-b)) return true;
+  }
+  if (moves[1]) {
+    if (x == a || y == b) return true;
+  }
+  if (moves[2]) {
+    if ((abs(x-a) == 1 && abs(y-b == 2)) || (abs(x-a == 2) && abs(y-b == 1))) return true;
+  }
+  if (moves[3]) {
+    if (abs(x-a) <= 2) return true;
+  }
+  if (moves[4]) {
+    if (abs(x-a) < 2 && abs(y-b) < 2) return true;
+  }
+  return false;
+}
+
+bool board::legal(int id, int x, int y, int a, int b) {
+  if (pos[7-x][y][1] == pos[7-a][b][1]) return false;
+  std::vector<bool> moves = getMoves(id);
+  if (moves[0]) {
+    if (abs(x-a) == abs(y-b)) return true;
+  }
+  if (moves[1]) {
+    if (x == a || y == b) return true;
+  }
+  if (moves[2]) {
+    if ((abs(x-a) == 1 && abs(y-b) == 2) || (abs(x-a)==2 && abs(y-b) == 1)) return true;
+  }
+  if (moves[3]) {
+    if (pos[x][y][1] == 2) {
+       if (x == 6) return (abs(x-a) <=2);
+       else return abs(x-a) ==1;
+    }
+    else {
+      if (x == 1) return (abs(x-a) <= 2);
+      else return abs(x-a) == 1;
+    }
+  }
+  if (moves[4]) {
+    if (abs(x-a) < 2 && abs(y-b) < 2) return true;
+  }
+  return false;
+}
+
+void board::swp(int x, int y, int a, int b) {
+  if (legal(pos[7-x][y][0],x,y,a,b)) {
+    a = 7-a;
+    // b = 7-b;
+    x = 7-x;
+    // y = 7-y; //orients coordinates to lower left
+    pos[a][b][0] = pos[x][y][0];
+    pos[a][b][1] = pos[x][y][1];
+    pos[x][y][0] = pos[x][y][1] = 0;
+  } else {
+    std::cout << "\n   Illegal move \n\n";
+    std::cout << "   For debugging, here's some info: " << pos[x][y][0] << " " << x << " " << y << "\n\n" ;
+  }
+}
+
 std::string getName(int a) {
   switch (a) {
     case 1:
@@ -39,10 +148,11 @@ std::string getName(int a) {
       break;
   }
 }
+
 std::string board::printBoard() {
  std::string ret = "";
  for (int i = 0; i < 8; i++) {
-   ret+=std::to_string(i+1)+"  ";
+   ret+=std::to_string(7-i)+"  ";
    for (int a = 0; a < 8; a++) {
      if (pos[i][a][0]) {
        if (pos[i][a][1] == 1) {
@@ -56,9 +166,10 @@ std::string board::printBoard() {
    }
    ret+="\n";
  }
- ret += "\n   A B C D E F G H";
+ ret += "\n   0 1 2 3 4 5 6 7";
  return ret;
 }
+
 void board::startGame() {
  int tmp[8][8] = {
    {4, 3, 2, 6, 5, 2, 3, 4},
--- testing.cpp
+++ testing.cpp
@@ -6,4 +6,14 @@ int main() {
   board game;
   game.startGame();
   std::cout << game.printBoard() << "\n";
+  game.swp(0,1,2,0);
+  std::cout << game.printBoard() << "\n";
+  game.swp(1,1,3,1);
+  std::cout << game.printBoard() << "\n";
+  game.swp(3,1,4,1);
+  std::cout << game.printBoard() << "\n";
+  game.swp(2,0,3,1);
+  std::cout << game.printBoard() << "\n";
+  game.swp(2,0,4,1);
+  std::cout << game.printBoard() << "\n";
 }
