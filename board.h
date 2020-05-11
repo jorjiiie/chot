8703f3e571cb65a15fbd5fbb298cafd97e7ca8f5
knot-eye <ryanzhu.1@outlook.com>
2020-05-11 12:15:52 -0400

    add color to distinguish between two sides

--- board.h
+++ board.h
@@ -7,7 +7,7 @@
 
 #include <string>
 class board {
- int pos[8][8],atk[8][8];
+ int pos[8][8][2],atk[8][8];
  public:
    std::string printBoard(void);
    void startGame(void);
@@ -23,7 +23,7 @@ std::string getName(int a) {
       return "B";
       break;
     case 3:
-      return "K";
+      return "N";
       break;
     case 4:
       return "R";
@@ -32,7 +32,7 @@ std::string getName(int a) {
       return "Q";
       break;
     case 6:
-      return "G";
+      return "K";
       break;
     default:
       return " ";
@@ -42,13 +42,21 @@ std::string getName(int a) {
 std::string board::printBoard() {
  std::string ret = "";
  for (int i = 0; i < 8; i++) {
-   ret+=std::to_string(i+1)+" ";
+   ret+=std::to_string(i+1)+"  ";
    for (int a = 0; a < 8; a++) {
-     ret+=(getName(pos[i][a]) + " ");
+     if (pos[i][a][0]) {
+       if (pos[i][a][1] == 1) {
+         ret+=("\033[1;31m"+getName(pos[i][a][0])+"\033[0m" + " ");
+       } else {
+         ret+=("\033[1;34m"+getName(pos[i][a][0])+"\033[0m" + " ");
+       }
+     }
+     else
+       ret+= "x ";
    }
    ret+="\n";
  }
- ret += "  A B C D E F G H";
+ ret += "\n   A B C D E F G H";
  return ret;
 }
 void board::startGame() {
@@ -63,8 +71,12 @@ void board::startGame() {
    {4, 3, 2, 6, 5, 2, 3, 4}
  };
  for (int i = 0; i < 8; i++) {
+   int n = 1;
+   if (i > 1) n = 0;
+   if (i > 5) n = 2;
    for (int a = 0; a < 8; a++) {
-     pos[i][a] = tmp[i][a];
+     pos[i][a][0] = tmp[i][a];
+     pos[i][a][1] = n;
    }
  }
 }
